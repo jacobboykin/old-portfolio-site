480d2fd159881ac40dce832f256a3b57e2889ebe
Jacob Boykin <boykinmusic@gmail.com>
2017-01-15 00:00:49 -0500

    Add tasks for browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,68 @@
+'use strict';
+
 var gulp = require('gulp'),
-    pug = require('gulp-pug');
+    pug = require('gulp-pug'),
+    browserSync = require('browser-sync').create();
+
 
 /* ================
 // Compile Pug
 // ============= */
 
 gulp.task('pug', function() {
+
   return gulp.src('src/pug/index.pug')
-  .pipe(pug())
+  .pipe(pug({
+    pretty: true
+  }))
   .pipe(gulp.dest('./public/'));
+
 });
 
-gulp.task('default', ['pug']);
+
+/* ================
+// Sync Changes
+// ============= */
+
+gulp.task('browser-sync', function() {
+
+  browserSync.init({
+    ui: false,
+    server: './public/',
+    files: ["./public/*.html", "./public/css/*.css"],
+    notify: {
+      styles: {
+        top: 'auto',
+        bottom: '0',
+        padding: '4px',
+        fontSize: '12px',
+        borderBottomLeftRadius: '0'
+      }
+    }
+  });
+
+});
+
+
+/* ================
+// Watch Files
+// ============= */
+
+gulp.task('watch', function() {
+
+  gulp.watch(['src/pug/**/*.pug'], ['pug']);
+
+});
+
+
+/* ================
+// Gulp Task Sets
+// ============= */
+
+gulp.task('build', ['pug']);
+
+gulp.task('default', [
+  'build',
+  'watch',
+  'browser-sync'
+]);
